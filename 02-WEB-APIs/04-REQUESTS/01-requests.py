f4e1dc861658d8e7983b246a2d67f711580bfb94
Brian Mugo <brayomugo2015@gmail.com>
2021-12-02 13:05:12 +0300

    01-requests

--- 02-WEB-APIs/04-REQUESTS/01-requests.py
+++ 02-WEB-APIs/04-REQUESTS/01-requests.py
@@ -1,9 +1,13 @@
 import requests
 
 def main():
-    response = requests.get("https://api.exchangeratesapi.io/latest")
-    print("Status code : ", response.status_code)
-    print("Content Type: ", response.headers["Content-Type"])
+    response = requests.get("https://api.genderize.io?name=peter")
+    
+    if response.status_code != 200:
+        print("Status code : ", response.status_code)
+        raise Exception("There was an error")
+    data = response.json()
+    print("Json response : ", data)
 
 
 if __name__ == "__main__":
