42b5f6b0a6e554e554fdc06b30cd1bc618c70f80
Brian Mugo <brayomugo2015@gmail.com>
2021-12-14 17:17:49 +0300

    changes

--- 02-WEB-APIs/FIRST_API_DJANGO/onlinestore/products/urls.py
+++ 02-WEB-APIs/FIRST_API_DJANGO/onlinestore/products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ProductDetailView, ProductListView
 
 urlpatterns = [
-    path("products/<int=pk>/", ProductDetailView.as_view(), name="product-detail"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
     path("", ProductListView.as_view(), name="product-list") 
 ]
