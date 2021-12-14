d0571e99e87281ac628088a528111baad6b60cb3
Brian Mugo <brayomugo2015@gmail.com>
2021-12-14 16:32:46 +0300

    Additions in view.py file

--- 02-WEB-APIs/FIRST_API_DJANGO/onlinestore/products/views.py
+++ 02-WEB-APIs/FIRST_API_DJANGO/onlinestore/products/views.py
@@ -5,3 +5,11 @@ from .models import Product, Manufacturer
 
 # Create your views here.
 
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "products/product_detail.html"
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "products/product_list.html"
+
