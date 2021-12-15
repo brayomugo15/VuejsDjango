03063f4211fbd251f0409b40b5531826c5bff27c
Brian Mugo <brayomugo2015@gmail.com>
2021-12-15 16:51:46 +0300

    more changes

--- 02-WEB-APIs/FIRST_API_DJANGO/onlinestore/products/urls.py
+++ 02-WEB-APIs/FIRST_API_DJANGO/onlinestore/products/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import product_detail, product_list #ProductDetailView, ProductListView
+from .views import manufacturer_detail, manufacturer_list, product_detail, product_list #ProductDetailView, ProductListView
 
 urlpatterns = [
     #path("products/<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
     path("products/", product_list, name="product-list"),
     path("products/<int:pk>/", product_detail, name="product-detail"),
+
+    ## Competency test
+    path("manufacturer/", manufacturer_list, name="manufacturer-list"),
+    path("manufacturer/<int:pk>", manufacturer_detail, name="manufacturer-detail"),
 ]
